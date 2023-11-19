cbaff56f7b8a436dbf65f1ea2a5b80c7ab932fc1
s2000promax <s2000promax@gmail.com>
2023-11-20 01:01:02 +0400

    add swagger, app config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,42 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import appConfig from '../config/app.config';
+import { SwaggerModule } from '@nestjs/swagger';
+import swaggerConfig from '../config/swagger.config';
+import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalInterceptors();
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    allowedHeaders: [
+      'content-type',
+      'Access-Control-Allow-Credentials',
+      'Origin',
+      'X-Requested-With',
+      'Authorization',
+      'Accept',
+    ],
+    origin: appConfig().origins,
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    credentials: true,
+  });
+  app.use(bodyParser.json({ limit: '1mb' }));
+  app.use(bodyParser.urlencoded({ limit: '1mb', extended: true }));
+  app.use(cookieParser());
+  app.setGlobalPrefix('api');
+
+  if (process.env.VERCEL_NODE_ENV !== 'production') {
+    const document = SwaggerModule.createDocument(app, swaggerConfig());
+    SwaggerModule.setup('api-doc', app, document);
+  }
+
+  const port = appConfig().port;
+
+  await app.listen(port);
 }
 bootstrap();
